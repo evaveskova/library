afd9ef2fdae6239a2ef71bd264f8f666786d8fb4
KerronKing <king_of_palmiste@hotmail.com>
2019-12-19 14:33:35 -0400

    Fix lint errors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const myLibrary = [
-  { 
-    title: 'Harry Potter', 
-    author: 'J.K. Rowling', 
-    pages: 394, 
-    status: false 
-  }
+  {
+    title: 'Harry Potter',
+    author: 'J.K. Rowling',
+    pages: 394,
+    status: false,
+  },
 ];
 class Book {
   constructor(title, author, pages) {
@@ -19,9 +19,7 @@ let addToLibrary = ({title, author, pages}) => {
   let obj = new Book(title, author, pages);
   return myLibrary.push(obj);
 }
-/* eslint-enable */
 const bookForm = document.forms['book-form'];
-/*eslint-disable */
 bookForm.addEventListener('submit', function(e){
   e.preventDefault();
   const data = Object.fromEntries(new FormData(bookForm).entries());
@@ -30,52 +28,43 @@ bookForm.addEventListener('submit', function(e){
   bookForm.reset();
 });
 let render = (array) => {
-  /* eslint-enable */
   const area = document.getElementById('book-area');
-  /* eslint-disable */
   area.innerHTML = '';
   array.forEach((book, i) => {
-    /*eslint-enable */
     let div = document.createElement('div');
-    /*eslint-disable */
     div.classList.add('card');
     div.setAttribute('data-index', `${i}`);
-    /* eslint-enable */
+
     const heading = document.createElement('h1');
-    /* eslint-disable */
     heading.classList.add('card-title');
     heading.textContent = book.title;
     div.appendChild(heading);
-    /* eslint-enable */
+
     const writer = document.createElement('p');
-    /* eslint-disable */
     writer.textContent = `Author:  + ${book.author}`;
     div.appendChild(writer);
-    /* eslint-enable */
+
     const pgNo = document.createElement('p');
-    /* eslint-disable */
     pgNo.textContent = `No. of pages: ' + ${book.pages}`;
     div.appendChild(pgNo);
-    /* eslint-enable */
+
     const deleteBtn = document.createElement('button');
-    /* eslint-disable */
     deleteBtn.textContent = 'Delete';
     deleteBtn.classList.add('d-btn');
     div.appendChild(deleteBtn);
-    /* eslint-enable */
+
     const readSwitch = document.createElement('input');
-    /* eslint-disable */
     readSwitch.setAttribute('type', 'checkbox');
     readSwitch.setAttribute('data-check', `${i}`);
     readSwitch.classList.add('switch');
     div.appendChild(readSwitch);
-    /* eslint-enable */
+
     const switchlabel = document.createElement('label');
-    /* eslint-disable */
     switchlabel.classList.add('slider');
     switchlabel.textContent = 'Status: Not Read';
     div.appendChild(switchlabel);
     area.appendChild(div);
+    
     const deleteEntry = (entry) => {
       if (entry.classList.contains('d-btn')) {
         entry.parentElement.remove();
